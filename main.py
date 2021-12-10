7c01f9167b28049b6d292949364edd73945951e6
Julian Knudson <julianknudson@Julians-MacBook-Pro.local>
2021-12-10 00:54:05 -0500

    you need a high iq to get the e

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import re
 from problem1 import *
+from problem2 import *
+from problem3 import *
 
 if __name__ == '__main__':
     with open('behavior-performance.txt','r') as fil:
@@ -38,6 +40,7 @@ if __name__ == '__main__':
             s.append(int(parsedLine.group(12)))
    
     problem1(userIDs,fracSpent,fracComp,fracPaused,numPauses,avgPBR,numRWs,numFFs,s)
+    problem2(userIDs,fracSpent,fracComp,fracPaused,numPauses,avgPBR,numRWs,numFFs,s)
     
 
  
--- problem3.py
+++ problem3.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def problem3(userIDs = [], fracSpent = [], fracComp = [], fracPaused = [], numPauses = [], avgPBR = [], numRWs = [], numFFs = [], s = []):
+def problem3(fracSpent, fracComp, fracPaused, numPauses, avgPBR, numRWs, numFFs, s, 
+            fracSpentModel, fracCompModel, fracPausedModel, numPausesModel, avgPBRModel, numRWsModel, numFFsModel, sModel, 
+            fracSpentWeight, fracCompWeight, fracPausedWeight, numPausesWeight, avgPBRWeight, numRWsWeight, numFFsWeight, sWeight):
     degrees = [1, 2, 3, 4, 5]
